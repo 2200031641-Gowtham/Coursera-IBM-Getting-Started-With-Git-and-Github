c4f9dbca140f11637275f6e92f4f59212d14622d
Ala Gowtham Siva Kumar <2200031641@kluniversity.in>
2023-10-06 15:06:38 +0530

    Update simple_interest.sh

--- simple_interest.sh
+++ simple_interest.sh
@@ -1,1 +1,17 @@
-echo "Hello, World!"
+#!/bin/bash
+
+# Prompt the user for principal amount, rate, and time
+echo "Enter the principal amount:"
+read principal
+
+echo "Enter the annual interest rate (as a decimal):"
+read rate
+
+echo "Enter the time period (in years):"
+read time
+
+# Calculate simple interest
+interest=$(echo "$principal * $rate * $time" | bc)
+
+# Display the result
+echo "Simple Interest: $interest"
